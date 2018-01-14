a71ec13d0c2b37d0eaad711113e639e58244e3be
wendyzou02 <wendyzou02@yahoo.com>
2018-01-14 14:27:30 -0800

    Adds deploy script

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello Bill', function(done) {
-    request(app).get('/').expect('hello Bill', done);
+  it('respond with hello Wendy', function(done) {
+    request(app).get('/').expect('hello Wendy', done);
   });
 });
